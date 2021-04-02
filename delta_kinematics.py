05e3f22f6df8520f1999303fe2b1d988b26b5662
benjelloo <55926840+benjelloo@users.noreply.github.com>
2021-04-02 01:51:29 -0700

    added fsolve

--- delta_kinematics.py
+++ delta_kinematics.py
@@ -1,13 +1,14 @@
 import math
 import numpy as np
 from scipy.optimize import fsolve
+from dataclasses import dataclass
 
 def inverse_kinematics(x,y,z):
     # [0,0,0] is defined to be the centre of the baseplate
     forearm_endeff_point = [x,y+BotParams.end_effector_radius,z]
     bicep_baseplate_point = [0,BotParams.base_radius,0]
     
-    forearm_proj = math.sqrt(BotParams.forearm_length^2-x^2) #project forearm onto x=0 axis
+    forearm_proj = math.sqrt(BotParams.forearm_length**2-x**2) #project forearm onto x=0 axis
 
     # we want to find the intersection point between the bicep circle and the forearm circle, which will
     # be the location of the joint
@@ -16,19 +17,23 @@ def inverse_kinematics(x,y,z):
     # (y-bicep_baseplate_point_y)^2+(z-bicep_baseplate_point_z)^2=BotParams.bicep_length^2
     # (y-forearm_endeff_point_y)^2+(z-forearm_endeff_point_z)^2=forearm_proj^2
     # solve for y,z!
-    yj,zj = fsolve(equations,(base_radius,0),forearm_endeff_point,bicep_baseplate_point,forearm_proj)
-    print(equations(yj,zj))
+    yj,zj = fsolve(circle_intersect,(BotParams.base_radius,0),(forearm_endeff_point,bicep_baseplate_point,forearm_proj))
+    print(circle_intersect((yj,zj),forearm_endeff_point,bicep_baseplate_point,forearm_proj))
     print(yj,zj)
     return 0
 
-def equations(p,forearm_endeff_point,bicep_baseplate_point,forearm_proj):
+def circle_intersect(p,forearm_endeff_point,bicep_baseplate_point,forearm_proj):
     y,z=p
-    return((y-bicep_baseplate_point[1])^2+(z-bicep_baseplate_point[2])^2-BotParams.bicep_length^2,
-            (y-forearm_endeff_point[1])^2+(z-forearm_endeff_point[2])^2-forearm_proj^2)
+    return((y-bicep_baseplate_point[1])**2+(z-bicep_baseplate_point[2])**2-BotParams.bicep_length**2,
+            (y-forearm_endeff_point[1])**2+(z-forearm_endeff_point[2])**2-forearm_proj**2)
+
 @dataclass
 class BotParams:
-    base_radius: float # center of baseplate to servo
-    bicep_length: float
-    forearm_length: float
-    end_effector_radius: float 
-    height: float # from the ground to the servo
+    base_radius: float = 0.5 # center of baseplate to servo
+    bicep_length: float = 0.4
+    forearm_length: float =0.7
+    end_effector_radius: float =0.14
+    height: float=2 # from the ground to the servo
+
+inverse_kinematics(0,0.5,-0.5)
+
